ac7c01292594d5cb863731209b17cfc46a6b9988
Naledi Morena <nalediking24@gmail.com>
2024-10-21 14:30:26 +0200

    Update config.py

--- Curefind/config.py
+++ Curefind/config.py
@@ -1,1 +1,6 @@
+import os
 
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'Naledi Neo'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///medapp.db'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
