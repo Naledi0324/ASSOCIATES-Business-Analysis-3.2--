b49f2b4e925b8da14aabf4e2733bcc05feb78b44
Naledi Morena <nalediking24@gmail.com>
2024-10-28 19:38:57 +0200

    Create medication_model.py

---
+++ Curefind/website/ml_model/medication_model.py
@@ -0,0 +1,14 @@
+import tensorflow as tf
+from tensorflow.keras import layers, models
+
+def create_medication_adherence_model():
+    model = models.Sequential([
+        layers.Dense(64, activation='relu', input_shape=(10,)),
+        layers.Dense(32, activation='relu'),
+        layers.Dense(1, activation='sigmoid')
+    ])
+    model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+    return model
+
+def train_model(model, train_data, train_labels):
+    model.fit(train_data, train_labels, epochs=10, batch_size=8)
