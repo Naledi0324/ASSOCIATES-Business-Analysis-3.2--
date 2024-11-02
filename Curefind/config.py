c10a11623a0cec5b1c1be310b6e8081117529800
Naledi Morena <nalediking24@gmail.com>
2024-11-02 22:13:00 +0200

    Delete Curefind directory

--- Curefind/config.py
+++
@@ -1,7 +0,0 @@
-#the file that helps the website know where the database is and keeps secret information safe 
-import os
-
-class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'Naledi Neo'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///medapp.db'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
--- Curefind/main.py
+++
@@ -1,9 +0,0 @@
-#This is the main file that starts the website
-from website import create_app
-
-app = create_app()
-
-if __name__ == "__main__":
-    app.run(debug=True)
-
-
