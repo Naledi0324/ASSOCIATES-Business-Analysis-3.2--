23fda8cafc70c8405361fb11d874a63c0dacd8ff
Naledi Morena <nalediking24@gmail.com>
2024-11-02 22:12:40 +0200

    Delete Curefind/website directory

--- Curefind/website/ml_model/time_series.py
+++
@@ -1,9 +0,0 @@
-#this file analyse medication schedules with time series modelling
-import pandas as pd
-from statsmodels.tsa.arima.model import ARIMA
-
-def predict_medication_adherence(user_data):
-    adherence_data = pd.Series(user_data['adherence_times'])
-    model = ARIMA(adherence_data, order=(5, 1, 0))  # Example ARIMA order
-    model_fit = model.fit()
-    return model_fit.forecast(steps=7)
